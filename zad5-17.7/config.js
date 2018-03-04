7a4596204f72af83c892dde803423087c00e929f
Malgorzata Kudlak <m.kudlak@icloud.com>
2018-03-04 19:45:09 +0100

    Add files for exe and script, pub not working

---
+++ zad5-17.7/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': 'TWÓJ CLIENT ID',
+    'GOOGLE_CLIENT_SECRET': 'TWÓJ CLIENT SECRET',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
