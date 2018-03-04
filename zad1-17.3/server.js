62efa9d3cdfc0e53b9f871e6bd45c5df05bf9e3f
Malgorzata Kudlak <m.kudlak@icloud.com>
2018-03-04 14:14:25 +0100

    Add path and check if it is working with postman

--- zad1-17.3/server.js
+++ zad1-17.3/server.js
@@ -1,55 +1,32 @@
 var express = require('express');
+var app = express();
 var bodyParser = require('body-parser');
 var fs = require('fs')
-var app = express();
 var stringifyFile;
-var id = 123;
+
+path = require("path");
 
 app.use(bodyParser.json());
 
 
-//app.get('/:id', function (req, res) {
-//    res.send('Identyfikator, który został dopisany to ' + req.params.id);
-//});
 
-app.get('/getNote', function (req, res) {
+app.get('/getNote', function(req, res) {
     fs.readFile('./test.json', 'utf8', function(err, data) {
         if (err) throw err;
         stringifyFile = data
         res.send(data);
     });
 });
-
-app.post('/', function (req, res) {
-    console.log('Otrzymałem żądanie POST do strony głównej');
-    res.send('Hello POST!');
+app.post('/updateNote/:note', function (req, res) {
+    stringifyFile + req.params.note;
+    fs.writeFile('./test.json', stringifyFile, function(err) {
+        if (err) throw err;
+        res.send(req.params.note);
+        console.log('file updated');
+    });
 });
-
-//app.post('/', function (req, res) {
-//    console.log('Otrzymałem żądanie POST do strony głównej');
-//    res.send('Hello POST!');
-//});
-/*
-app.delete('/del_user', function (req, res) {
-    console.log('Otrzymałem żądanie DELETE do strony /del_user');
-    res.send('Hello DELETE!');
-});
-
-app.get('/list_user', function (req, res) {
-    console.log('Otrzymałem żądanie GET do strony /list_user');
-    res.send('Strona z listą użytkowników!');
-});
-
-app.get('/ab*cd', function(req, res) {
-    console.log('Otrzymałem żądanie GET do strony /ab*cd');
-    res.send('Wzór pasuje');
-});
-*/
 
 app.listen(3000, function() {
     console.log('Przykładowa aplikacja nasłuchuje na http://localhost:3000');
 });
 
-//app.use(function (req, res, next) {
-//    res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
-//});
